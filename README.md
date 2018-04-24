6b492bf48cf66a490de9c40f0b000f7e83dadc11
TheSteelGuy <collotuos0@gmail.com>
2018-04-24 12:51:23 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Coverage Status](https://coveralls.io/repos/github/TheSteelGuy/Hello/badge.svg?branch=master)](https://coveralls.io/github/TheSteelGuy/Hello?branch=master)
+
 # Hello
---
+++ app/authenticate.py
@@ -0,0 +1,36 @@
+# app/authenticate.py
+# coding:utf-8
+
+# third party imports
+from flask import current_app, request, jsonify, make_response
+from functools import wraps
+# local imports
+from app.models import User
+
+
+def token_required(func):
+    """ modifyies the authenication token function in the database models."""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        auth_header = request.headers.get('Authorization')
+        if auth_header is None:
+            return make_response(jsonify(
+                {'message': 'provide a token in the authorization header, please'}
+            )), 403
+        auth_token = auth_header.split("Bearer ")[1]
+        if auth_token:
+            user_id = User.decode_token(auth_token)
+            if not isinstance(user_id, str):
+                user_id = user_id
+            else:
+                response = {
+                    'message': user_id
+                }
+                return make_response(jsonify(response)), 401
+        else:
+            response = {
+                'message': 'invalid token detected'
+            }
+            return make_response(jsonify(response)), 401
+        return func(user_id=user_id, *args, **kwargs)
+    return wrapper
